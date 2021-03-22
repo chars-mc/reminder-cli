044f8ffd4462d6b049071cbafec4c9c3cc472bf8
Carlos Manuel <chars.dev@gmail.com>
2021-03-21 19:40:15 -0600

    add OS notification

--- notifier/notifier.js
+++ notifier/notifier.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { notify } = require("node-notifier");
 
 const app = express();
 // server settings
@@ -11,6 +12,24 @@ app.use(express.json());
 // Sends http status 200 if server is running
 app.get("/health", (req, res) => {
   res.status(200).send();
+});
+
+// Sends an OS notification
+app.post("/notify", (req, res) => {
+  const notifyContent = {
+    title: req.body.title || "Unknown title",
+    message: req.body.message || "Unknown message",
+    sound: true,
+    wait: true,
+    reply: true,
+    closeLabel: "Completed?",
+    timeout: 15,
+  };
+
+  notify(notifyContent, (err, response, reply) => {
+    // sendding the notify reply to the response
+    res.send(reply);
+  });
 });
 
 module.exports = { app };
