a097db3c5c415a989bfbd760b8f52c25903c60d9
Carlos Manuel <chars.dev@gmail.com>
2021-03-23 23:45:55 -0600

    add documentation to wrapError function

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package client
 
 import "fmt"
 
+// wrapError returns a custom error and the original error
 func wrapError(customMsg string, originalError error) error {
 	return fmt.Errorf("%s : %v", customMsg, originalError)
 }
